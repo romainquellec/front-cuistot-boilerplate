cf1d27555fcb9d8b72fd085c9f1330ea4bf3f5d3
Romain Quellec <romain.quellec@gmail.com>
2019-12-03 15:14:31 +0100

    up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,42 @@
-import React from 'react';
-import App from 'next/app';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import theme from '../src/theme';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
+import App from "next/app";
+import Router from "next/router";
+import React from "react";
+import { initGA, logPageView } from "../shared/analytics";
+import theme from "../src/theme";
 
 export default class MyApp extends App {
-    componentDidMount() {
-        // Remove the server-side injected CSS.
-        const jssStyles = document.querySelector('#jss-server-side');
-        if (jssStyles) {
-            jssStyles.parentElement!.removeChild(jssStyles);
-        }
+  public static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
     }
 
-    render() {
-        const { Component, pageProps } = this.props;
+    return { pageProps }
+  }
 
-        return (
-            <React.Fragment>
-                <Head>
-                    <title>My page</title>
-                </Head>
-                <ThemeProvider theme={theme}>
-                    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-                    <CssBaseline />
-                    <Component {...pageProps} />
-                </ThemeProvider>
-            </React.Fragment>
-        );
+  public componentDidMount() {
+    initGA();
+    logPageView();
+    Router.router.events.on("routeChangeComplete", logPageView);
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement!.removeChild(jssStyles);
     }
+  }
+
+  public render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
 }
